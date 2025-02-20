f9c583107b2475cd5173d6c6750b350e0d58ab5b
Dima Muzyka <102657913+dimamuzyka@users.noreply.github.com>
2025-02-20 13:02:07 +0000

    update

--- adv_landing_gr.sql
+++ adv_landing_gr.sql
@@ -1,9 +1,20 @@
-select
-    month,
-    'gr_by_traffic_type' as fact_category,
-    'adv_landing_gr' as metric,
-    cast(sum(cpo_revenue + cpl_revenue) as int) as revenue
-from yc_hive.dashboard_perfomance.slicer_v2_tp_fact
-where tp_partner_type = '{advertiser}'
-group by 1
-order by 1
+with revenue as (
+    select month, source_site_id, sum(revenue_split) as revenue_split
+    from yc_hive.datamart_perfomance.revenue_split_by_bid
+    where month >= date('2024-01-01')
+    group by month, source_site_id
+
+    union all
+
+    select month, source_site_id, sum(revenue_split) as revenue_split
+    from yc_hive.datamart_perfomance.cpl_revenue_by_bid
+    where month >= date('2024-01-01')
+    group by month, source_site_id
+)
+select month, sum(revenue_split) as revenue
+from revenue
+where source_site_id in (
+    select id from yc_hive.masterdata_general.sites_v2 where partner_type = '{advertiser}'
+)
+group by month
+order by month;
